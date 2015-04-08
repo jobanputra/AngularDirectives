24b51f216d236868ee7635d752c541bac6cb3008
jobanputra <lukkhabuyer@gmail.com>
2015-04-08 16:32:28 -0700

    Created new directive to encapsulate DOM manipulation logic.

---
+++ Website/src/Website/wwwroot/stateDisplayDirective.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    var stateDisplay = function () {
+        return {
+            restrict: "A",
+            link: function (scope, el, attrs) {
+                scope.$watch(attrs["stateDisplay"], function(newValue){
+                    switch (newValue) {
+                        case 0:
+                            el.css("background-color", "white");
+                            break;
+                        case 1:
+                            el.css("background-color", "yellow");
+                            break;
+                        case 2:
+                            el.css("background-color", "red");
+                            break;
+                    }
+                })
+            }
+        };
+    }
+
+    angular
+        .module("angularDirectives")
+        .directive("stateDisplay", stateDisplay);
+
+
+})();
--- Website/src/Website/wwwroot/userInfoCardDirective.js
+++ Website/src/Website/wwwroot/userInfoCardDirective.js
@@ -9,30 +9,6 @@
                 user: "=person",
                 initialCollapsed:"@collapsed" //passing values to isolated scope
             }, //isolated scope
-            link:function(scope, el, attrs){
-                scope.nextState = function () {
-                    scope.user.level++;
-                    if (scope.user.level > 2)
-                        scope.user.level = 0;
-                    setState();
-                };
-
-               function setState() {
-                    switch (scope.user.level) {
-                        case 0:
-                            el.find(".panel-body").css("background-color", "white");
-                            break;
-                        case 1:
-                            el.find(".panel-body").css("background-color", "yellow");
-                            break;
-                        case 2:
-                            el.find(".panel-body").css("background-color", "red");
-                            break;
-                    }
-                }
-
-                setState();
-            },
             controller: function ($scope) {
                 $scope.collapsed = ($scope.initialCollapsed === "true");
                 $scope.knightMe = function (user) {
@@ -46,6 +22,11 @@
                     if (idx > -1) {
                         $scope.user.friends.splice(idx, 1);
                     }
+                };
+                $scope.nextState = function () {
+                    $scope.user.level++;
+                    if ($scope.user.level > 2)
+                        $scope.user.level = 0;
                 };
             },
             
