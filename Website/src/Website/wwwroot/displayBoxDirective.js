224ce66c89b54a18216eeb10f29b8dcbca8c9cf0
jobanputra <lukkhabuyer@gmail.com>
2015-04-08 17:56:22 -0700

    Basic transclusion.

---
+++ Website/src/Website/wwwroot/displayBoxDirective.js
@@ -0,0 +1,20 @@
+﻿(function () {
+	var displayBox = function () {
+		return {
+			restrict: "E",
+			templateUrl: "displayBox.html",
+			controller: function ($scope) {
+				$scope.hidden = false;
+				$scope.close = function () {
+					$scope.hidden = true;
+				}
+			},
+			transclude: true
+		}
+	};
+	angular
+		.module("angularDirectives")
+		.directive("displayBox", displayBox);
+
+
+})();
