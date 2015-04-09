e59cd870db1c619c1997af2e4608bca5064cf046
jobanputra <lukkhabuyer@gmail.com>
2015-04-09 13:48:50 -0700

    More involved transclusion. Questionnaire directive.

--- Website/src/Website/wwwroot/mainCtrl.js
+++ Website/src/Website/wwwroot/mainCtrl.js
@@ -1,7 +1,10 @@
 ﻿(function () {
 
     var mainCtrl = function ($scope) {
-        $scope.message = "Hello from controller";
+    	$scope.message = "Hello from controller";
+    	$scope.answers = {
+			baseLocation:"Yavin 4",
+    	}
     };
 
     var module = angular.module("angularDirectives");
---
+++ Website/src/Website/wwwroot/myQuestiondirective.js
@@ -0,0 +1,17 @@
+﻿(function (undefined) {
+
+	var myQuestion = function () {
+		return {
+			restrict: "E",
+			transclude: true,
+			templateUrl: "myQuestion.html",
+			scope: {
+				questionText: "@q"
+			}
+		};
+	}
+	angular
+		.module("angularDirectives")
+		.directive("myQuestion", myQuestion);
+
+})();
